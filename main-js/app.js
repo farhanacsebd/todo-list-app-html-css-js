e782f65a8a066ef14b2573f31bf2cd853d29aa05
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-28 23:07:21 +0600

    li added in success message

--- main-js/app.js
+++ main-js/app.js
@@ -3,7 +3,21 @@ const container = document.querySelector('.container');
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('#inputTodo');
 const todoAddButton = document.querySelector('#addTodoButton');
-const todoLists = document.querySelector('#lists');
+const todoLists = document.getElementById('lists');
+const messageElement = document.getElementById('message');
+
+
+
+// showMessage 
+const showMessage = (text,status) => {
+    messageElement.textContent = text;
+    messageElement.classList.add(`bg-${status}`);
+    setTimeout(()=>{
+        messageElement.textContent = "";
+        messageElement.classList.remove(`bg-${status}`);
+    },1000);
+}
+
 
 // createTodo
 const createTodo = (todoId,todoValue) =>{
@@ -26,7 +40,8 @@ const addTodo = (event) =>{
 
     // unique id 
     const todoId = Date.now().toString();
-    createTodo(todoId,todoValue)
+    createTodo(todoId,todoValue);
+    showMessage("todo is added","success");
 }
 
 
