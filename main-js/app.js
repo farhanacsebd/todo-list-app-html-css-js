19f4962df05ec57d889faf12c581a1a85434cdc4
farhanacsebd <farhana.cse.bd@gmail.com>
2022-09-28 21:07:46 +0600

    find the element

--- main-js/app.js
+++ main-js/app.js
@@ -1,1 +1,7 @@
-console.log('hello');
+// find the elements
+const container = document.querySelector('.container');
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('#inputTodo');
+const todoAddButton = document.querySelector('#addTodoButton');
+
+
